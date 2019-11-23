d675ff1467475d8214c861f24c1ec0f4b08f905b
Boris Hiltmeyer <boris@hiltmeyer.de>
2019-11-23 01:26:13 +0100

    update Skript - clear korrigiert

---
+++ up2date.sh
@@ -0,0 +1,32 @@
+# Shellskript zum updaten
+# chmod +x /pfad/zu/mein_Skript - ausführbar machen
+# #!/bin/bash
+# #!/bin/sh
+#
+#
+#!/bin/bash
+
+echo clear
+echo "
+(1) System updaten / upgraden
+(2) noch frei"
+
+read -p "Auswahl treffen:" varauswahl
+
+case "$varauswahl" in
+	1|eins) echo clear
+	        echo "apt update als root" 
+	   	sudo apt-get update
+		echo "apt upgrade als root"
+		sudo apt-get upgrade -y
+		echo "apt dist-upgrade als root"
+		sudo apt-get dist-upgrade -y
+		echo "apt autoremove als root"
+		sudo apt-get autoremove
+		;;
+	2|zwei) echo "noch nicht belegt"
+		;;
+	*) echo "ZONK"
+		;;
+esac
+#
